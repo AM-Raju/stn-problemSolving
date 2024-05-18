06667953c4f02cbbde2c5f27ac1028d0473dd82d
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-05-18 17:47:19 +0600

    First Commit

---
+++ 01.task_01.js
@@ -0,0 +1,1 @@
+/* Task-01 : Create An Array Of Objects, Each Representing A Person With Properties Like Name, Age, And Gender. Write A Function To Filter Out All Females And Then Map The Remaining People To An Array Of Names. Print The Final Result. */
